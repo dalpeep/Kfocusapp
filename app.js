5beb4876c3ac0aa0d7a83f9f73f54fbf51b28363
dalpeep <dalpeep3@gmail.com>
2026-06-24 22:26:24 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1490,8 +1490,7 @@ ${b.video_url || b.youtube_url ? `
 <button type="button"
         class="biz-gallery-thumb video-thumb"
         onclick="openBusinessVideo('${esc(b.video_url || b.youtube_url)}')">
-    <div class="video-overlay">
-        ▶
+
     </div>
 </button>
 ` : ''}
